d73f2e8ddc510df05ee847945f0a3836e06d6cfc
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-12-10 22:56:40 +1100

    add static stuff

--- pages/client-side.js
+++ pages/client-side.js
@@ -6,7 +6,31 @@ const headers = {
   "Cache-Control": "no-cache",
 };
 
-const ClientSide = ({ pokemon }) => {
+const ClientSide = () => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const response = await axios.get(url, { headers });
+
+      const promises = response.data.results.map((result) => {
+        return axios.get(result.url, { headers });
+      });
+
+      const responses = await Promise.all(promises);
+
+      const pokeData = responses.map((r) => {
+        return {
+          name: r.data.name,
+          imgUrl: r.data.sprites.front_default,
+        };
+      });
+
+      setPokemon(pokeData);
+    };
+
+    fetchPokemon();
+  }, []);
   console.log(pokemon);
   return pokemon.map((poke) => {
     return (
@@ -17,28 +41,6 @@ const ClientSide = ({ pokemon }) => {
       </div>
     );
   });
-};
-
-export const getStaticProps = async () => {
-  const response = await axios.get(url, { headers });
-
-  const promises = response.data.results.map((result) => {
-    return axios.get(result.url, { headers });
-  });
-
-  const responses = await Promise.all(promises);
-
-  const pokeData = responses.map((r) => {
-    return {
-      name: r.data.name,
-      imgUrl: r.data.sprites.front_default,
-    };
-  });
-  return {
-    props: {
-      pokemon: pokeData,
-    },
-  };
 };
 
 export default ClientSide;
