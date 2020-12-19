e4111d18e0e43236b8335f142c93d31737081fa1
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-12-19 22:34:46 +1100

    add directory cp

---
+++ functions-dev/charge-card.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const matter = require("gray-matter");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const getProducts = () => {
+  const directory = `${process.cwd()}/content`;
+  const filenames = fs.readdirSync(directory);
+
+  const products = filenames.map((filename) => {
+    // read the file from fs
+    const fileContent = fs
+      .readFileSync(require(`${directory}/${filename}`))
+      .toString();
+    // pull out frontmatter => name
+    const { data } = matter(fileContent);
+
+    return data;
+  });
+
+  return products;
+};
+
+exports.handler = async (event, context) => {
+  const { cart } = JSON.parse(event.body);
+
+  const products = getProducts();
+
+  const cartWithProducts = cart.map(({ id, qty }) => {
+    const product = products.find((p) => p.id === id);
+    return {
+      ...product,
+      qty,
+    };
+  });
+
+  console.log(cartWithProducts);
+  // talking to Stripe
+  const lineItems = cartWithProducts.map((product) => ({
+    price_data: {
+      currency: "aud",
+      product_data: {
+        name: product.name,
+      },
+      unit_amount: product.price,
+    },
+    quantity: product.qty,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    mode: "payment",
+    success_url: `${process.env.URL}/success`,
+    cancel_url: `${process.env.URL}/cancelled`,
+  });
+
+  // charging the card
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      id: session.id,
+    }),
+  };
+};
